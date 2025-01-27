50e9f39de1d7d94e54f03699c103acd400b33c2e
Daryna Kopytko <dashindafox@gmail.com>
2025-01-27 20:10:58 +0200

    clone kickstarter-landing-page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,47 +1,47 @@
-"use strict";
+'use strict';
 
 // зміна мови
 function getChangeLanguage(language) {
-  const buttonEng = document.getElementById("EN");
-  const deskButtonEng = document.getElementById("desk-button-en");
-  const buttonUa = document.getElementById("UA");
-  const deskButtonUa = document.getElementById("desk-button-ua");
+  const buttonEng = document.getElementById('EN');
+  const deskButtonEng = document.getElementById('desk-button-en');
+  const buttonUa = document.getElementById('UA');
+  const deskButtonUa = document.getElementById('desk-button-ua');
 
-  if (language === "en") {
-    buttonUa.classList.remove("active-language");
-    deskButtonUa.classList.remove("active-language");
-    buttonEng.classList.add("active-language");
-    deskButtonEng.classList.add("active-language");
-  } else if (language === "ua") {
-    buttonEng.classList.remove("active-language");
-    deskButtonEng.classList.remove("active-language");
-    buttonUa.classList.add("active-language");
-    deskButtonUa.classList.add("active-language");
+  if (language === 'en') {
+    buttonUa.classList.remove('active-language');
+    deskButtonUa.classList.remove('active-language');
+    buttonEng.classList.add('active-language');
+    deskButtonEng.classList.add('active-language');
+  } else if (language === 'ua') {
+    buttonEng.classList.remove('active-language');
+    deskButtonEng.classList.remove('active-language');
+    buttonUa.classList.add('active-language');
+    deskButtonUa.classList.add('active-language');
   }
 }
 
 document
-  .querySelector(".actions-desk__buttons-en")
-  .addEventListener("click", () => getChangeLanguage("en"));
+  .querySelector('.actions-desk__buttons-en')
+  .addEventListener('click', () => getChangeLanguage('en'));
 
 document
-  .querySelector(".actions-desk__buttons-en")
-  .addEventListener("click", () => getChangeLanguage("en"));
+  .querySelector('.actions-desk__buttons-en')
+  .addEventListener('click', () => getChangeLanguage('en'));
 
 document
-  .querySelector(".actions-desk__buttons-ua")
-  .addEventListener("click", () => getChangeLanguage("ua"));
+  .querySelector('.actions-desk__buttons-ua')
+  .addEventListener('click', () => getChangeLanguage('ua'));
 
 document
-  .querySelector(".actions-desk__buttons-ua")
-  .addEventListener("click", () => getChangeLanguage("ua"));
+  .querySelector('.actions-desk__buttons-ua')
+  .addEventListener('click', () => getChangeLanguage('ua'));
 
 // зміна слайдy
-const sliders = document.querySelector(".slider__container");
-const slide = document.querySelectorAll(".slide");
-const previousButton = document.querySelector(".slider__button-prev");
-const nextButton = document.querySelector(".slider__button-next");
-const slidesCount = document.querySelector(".slider__counter-number");
+const sliders = document.querySelector('.slider__container');
+const slide = document.querySelectorAll('.slide');
+const previousButton = document.querySelector('.slider__button-prev');
+const nextButton = document.querySelector('.slider__button-next');
+const slidesCount = document.querySelector('.slider__counter-number');
 
 let currentSlide = 0;
 
@@ -63,63 +63,63 @@ function changeSlide(direction) {
   slidesCount.textContent = `0${currentSlide + 1}`;
 }
 
-nextButton.addEventListener("click", () => {
+nextButton.addEventListener('click', () => {
   changeSlide(currentSlide + 1);
 });
 
-previousButton.addEventListener("click", () => {
+previousButton.addEventListener('click', () => {
   changeSlide(currentSlide - 1);
 });
 
 // відкривання та закривання меню
-const openButton = document.querySelector(".burger-menu__open");
-const closeButton = document.querySelector(".burger-menu__close");
-const menu = document.querySelector(".menu");
+const openButton = document.querySelector('.burger-menu__open');
+const closeButton = document.querySelector('.burger-menu__close');
+const menu = document.querySelector('.menu');
 const body = document.body;
-const menuLinks = document.querySelectorAll(".nav__link");
+const menuLinks = document.querySelectorAll('.nav__link');
 
 function toggleButtons(isOpen) {
-  openButton.classList.toggle("rotated", isOpen);
-  openButton.style.display = isOpen ? "none" : "block";
-  closeButton.style.display = isOpen ? "block" : "none";
+  openButton.classList.toggle('rotated', isOpen);
+  openButton.style.display = isOpen ? 'none' : 'block';
+  closeButton.style.display = isOpen ? 'block' : 'none';
 }
 
 function openMenu() {
   toggleButtons(true);
-  menu.classList.add("menu--active");
-  body.classList.add("no-scroll");
+  menu.classList.add('menu--active');
+  body.classList.add('no-scroll');
 }
 
 function closeMenu() {
   toggleButtons(false);
-  menu.classList.remove("menu--active");
-  body.classList.remove("no-scroll");
+  menu.classList.remove('menu--active');
+  body.classList.remove('no-scroll');
 }
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
+  link.addEventListener('click', (e) => {
     e.preventDefault();
     closeMenu();
 
-    const targetId = link.getAttribute("href").substring(1);
+    const targetId = link.getAttribute('href').substring(1);
     const targetElement = document.getElementById(targetId);
 
     if (targetElement) {
       window.scrollTo({
         top: targetElement.offsetTop,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     }
   });
 });
 
-openButton.addEventListener("click", openMenu);
-closeButton.addEventListener("click", closeMenu);
+openButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
 
 // form
 document
-  .getElementById("questionsForm")
-  .addEventListener("submit", function (e) {
+  .getElementById('questionsForm')
+  .addEventListener('submit', function(e) {
     e.preventDefault();
     this.reset();
   });
